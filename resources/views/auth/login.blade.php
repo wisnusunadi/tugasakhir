b3614470b571e01b37c3e3c8b7da447cc85686da
wisnusunadi <barto7773@gmail.com>
2022-02-22 21:23:03 +0700

    Initial commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+ 
+@section('content')
+<div class="login-box">
+  <div class="login-logo">
+    <a href="/"><b>Laravel 5</b> Login</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
+ 
+      <form method="POST" action="{{ route('login') }}">
+        @csrf
+        <div class="input-group mb-3">
+          <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }} has-feedback" placeholder="Email" name="email" value="{{ old('email') }}" required>
+ 
+          @if ($errors->has('email'))
+              <span class="invalid-feedback" role="alert">
+                  <strong>{{ $errors->first('email') }}</strong>
+              </span>
+          @endif
+          <div class="input-group-append">
+              <span class="fa fa-envelope input-group-text"></span>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }} has-feedback" placeholder="Password" name="password" required>
+          @if ($errors->has('password'))
+              <span class="invalid-feedback" role="alert">
+                  <strong>{{ $errors->first('password') }}</strong>
+              </span>
+          @endif
+          <div class="input-group-append">
+              <span class="fa fa-lock input-group-text"></span>
+          </div>
+        </div>
+        <div class="row">
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      <p class="mb-0">
+        <a href="{{ route('register') }}" class="text-center">Register</a>
+      </p>
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
