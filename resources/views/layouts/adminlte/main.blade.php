dc6f2a436eb8ccf6f92546711ab942b9c1f08d0a
aadzhaan <33817970+aadzhaan@users.noreply.github.com>
2022-02-23 15:29:51 +0700

    beranda, jadwal

--- resources/views/layouts/adminlte/main.blade.php
+++ resources/views/layouts/adminlte/main.blade.php
@@ -7,42 +7,51 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('themes/plugins/font-awesome/css/font-awesome.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('themes/plugins/fontawesome-free/css/all.min.css') }}">
+  <!-- DataTable-->
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jq-3.6.0/dt-1.11.4/rg-1.1.4/datatables.min.css"/>
+  <!-- <link rel="stylesheet" href="{{ asset('themes/plugins/datatables/datatables.min.css') }}"> -->
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('themes/dist/css/adminlte.min.css') }}">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="{{ asset('themes/plugins/iCheck/flat/blue.css') }}">
-  <!-- Morris chart -->
-  <link rel="stylesheet" href="{{ asset('themes/plugins/morris/morris.css') }}">
-  <!-- jvectormap -->
-  <link rel="stylesheet" href="{{ asset('themes/plugins/jvectormap/jquery-jvectormap-1.2.2.css') }}">
-  <!-- Date Picker -->
-  <link rel="stylesheet" href="{{ asset('themes/plugins/datepicker/datepicker3.css') }}">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="{{ asset('themes/plugins/daterangepicker/daterangepicker-bs3.css') }}">
-  <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="{{ asset('themes/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('themes/plugins/daterangepicker/daterangepicker.css') }}">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
+@yield('custom_css')
+<style>
+  @font-face
+  {
+    font-family: 'Poppins';
+    src: url('assets/font/Poppins/Poppins-Regular.ttf');
+  }
+  body{
+    font-family: 'Poppins';
+  }
+  footer{
+    bottom: 0;
+    position: fixed;
+    width: 100%;
+  }
+  @media screen and (min-width:992px){
+    body{font-size: 14px;}
+  }
+
+  @media screen and (max-width:991px){
+    body{font-size: 12px;}
+  }
+</style>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
  
-  @include('layouts.adminlte.navbar')
- 
   @include('layouts.adminlte.sidebar')
- 
+  
   @yield('content')
  
- 
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2018 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.0.0-alpha
-    </div>
+    <div>Dibuat untuk Tugas Akhir 2022</div>
   </footer>
  
   <!-- Control Sidebar -->
@@ -56,28 +65,19 @@
 <!-- jQuery -->
 <script src="{{ asset('themes/plugins/jquery/jquery.min.js') }}"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+<!-- <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script> -->
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
 <script src="{{ asset('themes/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<!-- Morris.js charts -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<script src="{{ asset('themes/plugins/morris/morris.min.js') }}"></script>
-<!-- Sparkline -->
-<script src="{{ asset('themes/plugins/sparkline/jquery.sparkline.min.js') }}"></script>
-<!-- jvectormap -->
-<script src="{{ asset('themes/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
-<script src="{{ asset('themes/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
-<!-- jQuery Knob Chart -->
-<script src="{{ asset('themes/plugins/knob/jquery.knob.js') }}"></script>
+<!-- DataTable-->
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jq-3.6.0/dt-1.11.4/rg-1.1.4/datatables.min.js"></script>
+<!-- <link rel="stylesheet" href="{{ asset('themes/plugins/datatables/datatables.min.js') }}" type="text/javascript"> -->
 <!-- daterangepicker -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js"></script>
 <script src="{{ asset('themes/plugins/daterangepicker/daterangepicker.js') }}"></script>
-<!-- datepicker -->
-<script src="{{ asset('themes/plugins/datepicker/bootstrap-datepicker.js') }}"></script>
 <!-- Bootstrap WYSIHTML5 -->
 <script src="{{ asset('themes/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}"></script>
 <!-- Slimscroll -->
@@ -88,9 +88,8 @@
 <script src="{{ asset('themes/dist/js/adminlte.js') }}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="{{ asset('themes/dist/js/pages/dashboard.js') }}"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="{{ asset('themes/dist/js/demo.js') }}"></script>
- 
+ <!-- Font Awesome -->
+ <link rel="stylesheet" href="{{ asset('themes/plugins/fontawesome-free/js/all.min.js') }}">
 @yield('script')
 </body>
 </html>
