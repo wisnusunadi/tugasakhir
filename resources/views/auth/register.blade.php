756f1fc2bf3b9cfd082edadf4a314485b13fc742
wisnusunadi <barto7773@gmail.com>
2022-03-24 11:15:11 +0700

    tes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -239,8 +239,6 @@
                                         </button>
                                     </div>
                                 </div>
-
-
                         </div>
                     </div>
 
